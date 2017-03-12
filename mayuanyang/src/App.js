5e7b21e21ca943cc8608013d5e15f512036a5f49
Eddy Ma <eddy.ma@smsmt.com>
2017-03-12 12:05:46 +1000

    simple update to my page

--- mayuanyang/src/App.js
+++ mayuanyang/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AboutMe from './AboutMe';
+
 
 class App extends Component {
   render() {
@@ -8,11 +10,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <AboutMe />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+        I will update this page every so often
+        </div>
       </div>
     );
   }
